608a9c71bda5daa7636ccf460294586ee407eba9
Alex Calvert <alexanderajcalvert@gmail.com>
2021-06-14 11:18:12 -0700

    commit tree 97ce48f2849e43bdd9054b1f8639a699ad08fa2f

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
+import { cleanup } from "@testing-library/react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    let timer = setTimeout(function() {
+      setTimeRemaining(timeRemaining - 1)
+    }, 1000)
+
+    if (timeRemaining === 0) {
+      onAnswered(false)
+      setTimeRemaining(10)
+    }
+
+    return function cleanup() {
+      clearTimeout(timer);
+    }  
+
+  },
+  )
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
